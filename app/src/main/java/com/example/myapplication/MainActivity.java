89d17741408f30092b04cefc7c97d7daf1778ed5
mskat <cse242017011@uniwa.gr>
2022-05-03 00:00:56 +0300

    added functionality to sound button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,11 +16,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.button_begin);
+        Button button2 = (Button) findViewById(R.id.button_sound_on_off);
+
+
         button.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, MainMenu.class);
             startActivity(intent);
         });
+        final MediaPlayer media = MediaPlayer.create(this, R.raw.street_soul);
+        media.setLooping(true);
+        button2.setOnClickListener(v -> {
+
+                if(media.isPlaying()==false) {
+                    button2.setText(R.string.app_on_mus);
+                    media.start();
+
+                }
+                else {
+                    media.pause();
+                    button2.setText(R.string.app_off_mus);
+                }
+        });
 
     }
-
 }
