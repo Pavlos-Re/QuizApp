92ba1824fef18024e5a372e8abb91a1e2a8bfd98
Pavlos-Re <pavlos.repin@gmail.com>
2022-05-02 18:37:10 +0300

    Introductory App Scene nearly done - music left to be added.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button_begin);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, MainMenu.class);
+            startActivity(intent);
+        });
+
     }
+
 }
