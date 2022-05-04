0529b271f99b4b2f6b53aee2578c8943ca4cd03a
Pavlos-Re <pavlos.repin@gmail.com>
2022-05-04 22:55:19 +0300

    Main Menu buttons customization done. - Minor problem with the music.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,11 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -22,22 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final MediaPlayer media2 = MediaPlayer.create(MainActivity.this, R.raw.pop );
-         media = MediaPlayer.create(this, R.raw.street_soul);
+        media = MediaPlayer.create(this, R.raw.street_soul);
         button = findViewById(R.id.button_begin);
         button2 = findViewById(R.id.button_sound_on_off);
-
-
-
-
-
-
 
         button.setOnClickListener(v -> {
             media2.start();
             Intent intent = new Intent(MainActivity.this, MainMenu.class);
             startActivity(intent);
         });
-
 
         media.setLooping(true);
         button2.setOnClickListener(v -> {
@@ -46,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
                 button2.setText(R.string.app_on_mus);
                 media2.start();
                 media.start();
-
             } else {
                 media.pause();
                 media2.start();
@@ -56,23 +46,25 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-@Override
-protected void onPause() {
-    super.onPause();
-    if (media != null) media.pause();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("Status: OnPause");
+    }
 
-}
-@Override
-protected void onResume() {
+    @Override
+    protected void onResume() {
         super.onResume();
-       button2 = findViewById(R.id.button_sound_on_off);
-       button2.setText(R.string.app_off_mus);
-
-}
+        button2 = findViewById(R.id.button_sound_on_off);
+        button2.setText(R.string.app_off_mus);
+        System.out.println("Status: OnResume");
+    }
 
     @Override
     public void onBackPressed() {
+        System.out.println("Status: OnBackPressed");
         if (backButtonCount >= 1) {
+            if (media != null) media.pause();
             Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_HOME);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -84,4 +76,15 @@ protected void onResume() {
         }
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        System.out.println("Status: OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("Status: OnDestroy");
+    }
 }
