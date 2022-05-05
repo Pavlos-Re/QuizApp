3f289a296ba52df6f919f331261fb12049ca4c92
pavlosre <pavlos.repin@gmail.com>
2022-05-05 06:38:57 +0300

    SubjectList Menu nearly done.

--- app/src/main/java/com/example/myapplication/HighScores.java
+++ app/src/main/java/com/example/myapplication/HighScores.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class High_Scores extends AppCompatActivity {
+public class HighScores extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/myapplication/SubjectList.java
+++ app/src/main/java/com/example/myapplication/SubjectList.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class SubjectList extends AppCompatActivity {
 
@@ -10,5 +14,54 @@ public class SubjectList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
+
+
+        ImageView computerVision = (ImageView) findViewById(R.id.computer_vision_icon);
+        ImageView maths = (ImageView) findViewById(R.id.maths_icon);
+        ImageView physics = (ImageView) findViewById(R.id.physics_icon);
+        ImageView databases = (ImageView) findViewById(R.id.data_bases_icon);
+        ImageView internetSecurity = (ImageView) findViewById(R.id.internet_security_icon);
+
+        computerVision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SubjectList.this,
+                        "You choose Computer Vision",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        maths.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SubjectList.this,
+                        "You choose Maths",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        physics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SubjectList.this,
+                        "You choose Physics",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        databases.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SubjectList.this,
+                        "You choose Databases",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        internetSecurity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SubjectList.this,
+                        "You choose Internet Security",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
