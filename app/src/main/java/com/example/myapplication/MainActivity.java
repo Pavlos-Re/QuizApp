6ddd6915a081cda0f16dd92e63d1acf9fa26e67c
mskat <cse242017011@uniwa.gr>
2022-05-06 15:35:50 +0300

    Added Calculator

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,11 +1,26 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     MediaPlayer media;
@@ -15,16 +30,36 @@ public class MainActivity extends AppCompatActivity {
     Button button4;
     int backButtonCount = 0;
     boolean homePressed = false;
+    private static final int RESULT_LOAD_IMAGE = 5164654;;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ArrayList<HashMap<String,Object>> items =new ArrayList<HashMap<String,Object>>();
+
+        final PackageManager pm = getPackageManager();
+        List<PackageInfo> packs = pm.getInstalledPackages(0);
+        for (PackageInfo pi : packs) {
+            if( pi.packageName.toString().toLowerCase().contains("calcul")){
+                HashMap<String, Object> map = new HashMap<String, Object>();
+                map.put("appName", pi.applicationInfo.loadLabel(pm));
+                map.put("packageName", pi.packageName);
+                items.add(map);
+            }
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Drawable d = Drawable.createFromPath(R.drawable.databases);
         final MediaPlayer media2 = MediaPlayer.create(MainActivity.this, R.raw.pop );
         media = MediaPlayer.create(this, R.raw.street_soul);
         button = findViewById(R.id.button_begin);
         button2 = findViewById(R.id.button_sound_on_off);
         button3 = findViewById(R.id.button_about);
+        button4 = findViewById(R.id.button_calc);
+
+
+
 
         button.setOnClickListener(v -> {
             media2.start();
@@ -36,6 +71,23 @@ public class MainActivity extends AppCompatActivity {
             Intent intent2 = new Intent(MainActivity.this, About.class);
             startActivity(intent2);
         });
+
+
+        button4.setOnClickListener(v -> {
+            if(items.size()>=1){
+                media2.start();
+                String packageName = (String) items.get(0).get("packageName");
+                Intent i = pm.getLaunchIntentForPackage(packageName);
+                if (i != null)
+                    startActivity(i);
+            }
+            else{Toast.makeText(MainActivity.this,
+                    "Calculator App does not exist",
+                    Toast.LENGTH_SHORT).show();
+                    media2.start();
+            }
+
+                });
 
         media.setLooping(true);
         button2.setOnClickListener(v -> {
@@ -52,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
 
     @Override
     protected void onPause() {
